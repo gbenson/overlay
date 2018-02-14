af21dc4990fce9ef8951273c188d1f0165a44087
Gary Benson <gbenson@redhat.com>
2018-02-14 21:29:13 +0000

    s4i overlayer

---
+++ overlay.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from PIL import Image
+import numpy
+import os
+
+def _filenames(topdir, prefix, suffix):
+    for dirpath, dirnames, filenames in os.walk(topdir):
+        for filename in filenames:
+            if not filename.startswith(prefix):
+                continue
+            if not filename.endswith(suffix):
+                continue
+            index = filename[len(prefix):-len(suffix)]
+            try:
+                index = int(index)
+            except ValueError:
+                continue
+            yield index, os.path.join(dirpath, filename)
+
+def filenames(*args):
+    unique = {}
+    for index, filename in _filenames(*args):
+        assert not index in unique
+        unique[index] = filename
+    for index, filename in sorted(unique.items()):
+        yield filename
+
+class Overlayer(object):
+    def __init__(self, size):
+        self.totals = None
+
+    def push(self, filename):
+        print(filename)
+        index = int(filename[-8:-4])
+        img = Image.open(filename)
+        if self.totals is None:
+            self.camera = filename.split(os.sep, 5)[-2]
+            self.__size = img.size
+            self.totals = numpy.zeros((self.__size[1], self.__size[0], 3),
+                                      dtype=numpy.int64)
+        if img.size != self.__size:
+            scale = [i / s for i, s in zip(img.size, self.__size)]
+            if scale[0] != scale[1] and img.size != (self.__size[0] - 1,
+                                                     self.__size[1] - 1):
+                print(img.size, scale)
+                raise AssertionError
+            img = img.resize(self.__size, Image.ANTIALIAS)
+        self.totals += numpy.array(img)
+        self.__last = index
+
+    def output(self):
+        img = self.totals.copy()
+        tmp = img.min()
+        print("min =", tmp,)
+        img -= tmp
+        tmp = img.max()
+        print("max =", tmp)
+        img *= 255
+        img //= tmp
+        img = Image.frombytes("RGB", self.__size,
+                              img.astype(numpy.int8).tostring())
+        img.save("%s-%04d.png" % (self.camera, self.__last))
+
+if __name__ == "__main__":
+    ol = Overlayer((640, 480))
+    for filename in filenames(os.path.join(os.environ["HOME"],
+                                           "Pictures", "s4i"),
+                              "imgp", ".jpg"):
+        try:
+            ol.push(filename)
+        except:
+            break
+    else:
+        print("All done!")
+    ol.output()
